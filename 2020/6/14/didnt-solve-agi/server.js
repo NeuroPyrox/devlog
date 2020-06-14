671430940f0b77b2242ff88dec2874c554dc9532
Alex Montoya <NeuroPyrox@Gmail.com>
2020-06-14 16:37:43 -0600

    Didn't solve agi (#13)
    * refactoring
    
    * Didn't solve agi
    
    Co-authored-by: Glitch (neuropyrox) <none>

---
+++ 2020/6/14/didnt-solve-agi/server.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = require("../../../5/21/htmlResponse.js")(__dirname);
--- server.js
+++ server.js
@@ -1,30 +1,41 @@
 "use strict";
 
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
-
-const requireFile = filePath => require(path.join(__dirname, filePath));
+const fs = require("fs").promises;
+const requireFile = filePath => require(`${__dirname}/${filePath}`);
 
 const paths = requireFile("2020/5/oldHandlers.js");
 paths["/"] = requireFile("2020/5/20/homepage/server.js");
 
-const directory = [
-  [21, "progress-report"],
-  [21, "previous-blog"],
-  [21, "file-structure"],
-  [21, "echo"],
-  [21, "graph"],
-  [22, "nested-hover"],
-  [22, "s-expression-sprites"],
-  [27, "progress-report"]
-];
+// WARNING: this is hardcoded instead of general
+const removeNonFolders = items => {
+  const index = items.indexOf("htmlResponse.js");
+  if (index !== -1) {
+    items.splice(index, 1);
+  }
+};
 
-for (const [day, page] of directory) {
-  const urlPath = `/2020/5/${day}/${page}`;
-  const filePath = `2020/5/${day}/${page}/server.js`;
-  paths[urlPath] = requireFile(filePath);
-}
+const getHandlersFromFolder = async handlerFolder => {
+  const items = await fs.readdir(`${__dirname}/2020/5/${handlerFolder}`);
+  removeNonFolders(items);
+  const handlers = items.map(item => [
+    `/2020/5/${handlerFolder}/${item}`,
+    requireFile(`2020/5/${handlerFolder}/${item}/server.js`)
+  ]);
+  return handlers;
+};
+
+const addHandlers = async () => {
+  // WARNING: [21, 22, 27] is hardcoded instead of general
+  const promises = [21, 22, 27]
+    .map(getHandlersFromFolder)
+    .map(async handlers => {
+      for (const [urlPath, handler] of await handlers) {
+        paths[urlPath] = handler;
+      }
+    });
+  return Promise.all(promises);
+};
 
 const handle404error = res => {
   res.writeHead(404, { "Content-Type": "text/html" });
@@ -32,6 +43,7 @@ const handle404error = res => {
   res.end();
 };
 
+// WARNING: this is hardcoded instead of general
 const isEchoUrl = url => url.startsWith("/2020/5/21/echo/");
 const handleEcho = (req, res) => {
   const echo = req.url.slice(16);
@@ -42,20 +54,29 @@ const handleEcho = (req, res) => {
   res.end();
 };
 
-http
-  .createServer((req, res) => {
-    if (isEchoUrl(req.url)) {
-      handleEcho(req, res);
-      return;
-    }
+const main = async () => {
+  await addHandlers();
+  paths["/2020/6/14/didnt-solve-agi"] = requireFile(
+    `2020/6/14/didnt-solve-agi/server.js`
+  );
+  http
+    .createServer((req, res) => {
+      // WARNING: this if statement is hardcoded instead of general
+      if (isEchoUrl(req.url)) {
+        handleEcho(req, res);
+        return;
+      }
 
-    const handler = paths[req.url];
-    if (handler === undefined) {
-      handle404error(res);
-    } else {
-      handler(res);
-    }
-  })
-  .listen(process.env.PORT, () => {
-    console.log(`Your app is listening on port ${process.env.PORT}`);
-  });
+      const handler = paths[req.url];
+      if (handler === undefined) {
+        handle404error(res);
+      } else {
+        handler(res);
+      }
+    })
+    .listen(process.env.PORT, () => {
+      console.log(`Your app is listening on port ${process.env.PORT}`);
+    });
+};
+
+main();
