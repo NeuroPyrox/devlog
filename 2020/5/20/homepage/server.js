a4f45b167820872e06a8f199a7869803fc9e0b58
Glitch (neuropyrox) <none>
2020-06-15 02:00:58 +0000

    Refactor

--- 2020/5/20/homepage/server.js
+++ 2020/5/20/homepage/server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const getMayPosts = require("../post-finder/getMayPosts.js");
+const getPostLocations = require("../../getPostLocations.js");
 
 const templateHtml = listHtml => `
   <!DOCTYPE html>
@@ -70,30 +70,9 @@ const makeItem = (day, title, href) => `
   </a>
 `;
 
-const oldPosts = [
-  { day: 20, title: "20th", href: "/2020/5/20/" },
-  { day: 19, title: "19th", href: "/2020/5/19/" },
-  { day: 17, title: "17th", href: "/2020/5/17/" },
-  { day: 17, title: "Pills", href: "/2020/5/17/pills" },
-  { day: 17, title: "Drag N Drop", href: "/2020/5/17/dragNDrop" },
-  { day: 17, title: "Mobile Drag N Drop", href: "/2020/5/17/mobileDragNDrop" },
-  { day: 17, title: "Follow Mouse", href: "/2020/5/17/followMouse" },
-  { day: 16, title: "Simple Homepage", href: "/2020/5/16/" },
-  { day: 15, title: "May Folders", href: "/2020/5/15/" },
-  { day: 14, title: "Hello World", href: "/2020/5/14/" }
-];
-
 const makeHtml = async () => {
-  const mayPosts = (await getMayPosts())
-    .map(({ day, title }) => ({
-      day: day,
-      title: title,
-      href: `/2020/5/${day}/${title}`
-    }))
-    .reverse();
-  const posts = mayPosts.concat(oldPosts);
-  const listHtml = posts
-    .map(({ day, title, href }) => makeItem(day, title, href))
+  const listHtml = (await getPostLocations())
+    .map(({ day, title, href }) => makeItem(day, title, "2020/5/" + href))
     .join();
   return templateHtml(listHtml);
 };
---
+++ 2020/5/getPostLocations.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const getMayPosts = require("./20/post-finder/getMayPosts.js");
+
+const oldPosts = [
+  { day: 20, title: "20th", href: "20/" },
+  { day: 19, title: "19th", href: "19/" },
+  { day: 17, title: "17th", href: "17/" },
+  { day: 17, title: "Pills", href: "17/pills" },
+  { day: 17, title: "Drag N Drop", href: "17/dragNDrop" },
+  { day: 17, title: "Mobile Drag N Drop", href: "17/mobileDragNDrop" },
+  { day: 17, title: "Follow Mouse", href: "17/followMouse" },
+  { day: 16, title: "Simple Homepage", href: "16/" },
+  { day: 15, title: "May Folders", href: "15/" },
+  { day: 14, title: "Hello World", href: "14/" }
+];
+
+module.exports = async () => {
+  const mayPosts = (await getMayPosts())
+    .map(({ day, title }) => ({
+      day: day,
+      title: title,
+      href: `${day}/${title}`
+    }))
+    .reverse();
+  return mayPosts.concat(oldPosts);
+};
