db1b7d432083db3ca091f2363e0f4ea8da0a04c5
Glitch (neuropyrox) <none>
2020-06-27 19:13:07 +0000

    Homepage

---
+++ 2020/6/27/homepage/server.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const getPosts = require("../get-posts/getPosts.js");
+
+const templateList = listHtml => `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title>NeuroPyrox's Blog</title>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style>
+        @import url("https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap");
+
+        body {
+          background-color: black;
+          font-family: "Nova Mono", monospace;
+        }
+
+        a:link,
+        a:visited {
+          color: green;
+          text-decoration: none;
+        }
+
+        .post {
+          border-radius: 20px;
+          padding: 10px;
+        }
+
+        .post:hover {
+          background-color: #020;
+          cursor: pointer;
+        }
+
+        h2 {
+          margin: 0;
+        }
+
+        h3 {
+          margin: 0;
+          text-align: right;
+        }
+
+        .list {
+          max-width: 700px;
+          margin: 0 auto;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="list">
+        ${listHtml}
+      </div>
+    </body>
+  </html>
+`;
+
+const templatePost = ({date, title, href}) => `
+  <a href="${href}">
+    <div class="post">
+      <h2>
+        ${title}
+      </h2>
+      <h3>
+        ${date}
+      </h3>
+    </div>
+  </a>
+`;
+
+const templateHtml = posts => templateList(posts.map(templatePost).join());
+
+const makeHtml = async (juneUrl, mayUrl, mayPosts) => templateHtml(await getPosts(juneUrl, mayUrl, mayPosts));
+
+module.exports = (juneUrl, mayUrl, mayPosts) => {
+  const html = makeHtml(juneUrl, mayUrl, mayPosts);
+  return async res => {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+    res.write(await html);
+    res.end();
+  };
+};
--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const juneUrl = "/2020/6";
 
 const mayPosts = require("./2020/5/getPostLocations.js")();
 
+const homepage = require("./2020/6/27/homepage/server.js")(juneUrl, mayPosts, mayUrl);
+
 // HARDCODED
 const paths = {
-  "/": require("./2020/5/20/homepage/server.js")(mayUrl),
+  "/": homepage,
   "/2020/6/14/didnt-solve-agi": require("./2020/6/14/didnt-solve-agi/server.js"),
   "/2020/6/15/post-locations": require("./2020/6/15/post-locations/server.js")(juneUrl, mayPosts, mayUrl),
   "/2020/6/22/get-june-days": require("./2020/6/22/get-june-days/server.js"),
   "/2020/6/27/get-june-posts": require("./2020/6/27/get-june-posts/server.js")(juneUrl),
-  "/2020/6/27/get-posts": require("./2020/6/27/get-posts/server.js")(juneUrl, mayPosts, mayUrl)
+  "/2020/6/27/get-posts": require("./2020/6/27/get-posts/server.js")(juneUrl, mayPosts, mayUrl),
+  "/2020/6/27/homepage": homepage
 }
 
 const handle404error = res => {
