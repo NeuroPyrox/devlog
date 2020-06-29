22107fda73a111760535cea16691c934f7fa963c
Glitch (neuropyrox) <none>
2020-06-29 20:52:18 +0000

    Ignore empty files

--- 2020/6/27/get-june-posts/getJunePosts.js
+++ 2020/6/27/get-june-posts/getJunePosts.js
@@ -4,15 +4,21 @@ const fs = require("fs").promises;
 
 const getJuneDays = async () => fs.readdir(__dirname + "/../..");
 
-const getPostFilesFromDay = async day =>
-  fs.readdir(__dirname + "/../../" + day);
+const getPostFoldersFromDay = async day => {
+  const folders = await fs.readdir(__dirname + "/../../" + day);
+  const nested = await Promise.all(
+    folders.map(folder => fs.readdir(`${__dirname}/../../${day}/${folder}`))
+  );
+  const isEmpty = nested.map(items => items.length === 0);
+  return folders.filter((_, i) => !isEmpty[i]);
+};
 
 const getPostsOnDay = async (juneUrl, day) => {
-  const files = await getPostFilesFromDay(day);
-  return files.map(fileName => ({
+  const folders = await getPostFoldersFromDay(day);
+  return folders.map(name => ({
     date: `${day} June 2020`,
-    title: fileName,
-    href: `${juneUrl}/${day}/${fileName}`
+    title: name,
+    href: `${juneUrl}/${day}/${name}`
   }));
 };
 
@@ -22,4 +28,4 @@ module.exports = async juneUrl => {
     juneDays.map(day => getPostsOnDay(juneUrl, day))
   );
   return posts.flat();
-}
+};
