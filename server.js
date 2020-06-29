fc6a79fba59a817eb0a96601620112b62de28ecc
Glitch (neuropyrox) <none>
2020-06-29 16:00:57 +0000

    Url Tail

--- server.js
+++ server.js
@@ -7,19 +7,31 @@ const juneUrl = "/2020/6";
 
 const mayPosts = require("./2020/5/getPostLocations.js")();
 
-const juneContext = {juneUrl: juneUrl, mayPosts: mayPosts, mayUrl: mayUrl};
+const juneContext = { juneUrl: juneUrl, mayPosts: mayPosts, mayUrl: mayUrl };
 
 // HARDCODED
 const paths = {
   "/": require("./2020/6/27/homepage/server.js")(juneContext),
-  "/2020/6/14/didnt-solve-agi": require("./2020/6/14/didnt-solve-agi/server.js")(juneContext),
-  "/2020/6/15/post-locations": require("./2020/6/15/post-locations/server.js")(juneContext),
-  "/2020/6/22/get-june-days": require("./2020/6/22/get-june-days/server.js")(juneContext),
-  "/2020/6/27/get-june-posts": require("./2020/6/27/get-june-posts/server.js")(juneContext),
-  "/2020/6/27/get-posts": require("./2020/6/27/get-posts/server.js")(juneContext),
+  "/2020/6/14/didnt-solve-agi": require("./2020/6/14/didnt-solve-agi/server.js")(
+    juneContext
+  ),
+  "/2020/6/15/post-locations": require("./2020/6/15/post-locations/server.js")(
+    juneContext
+  ),
+  "/2020/6/22/get-june-days": require("./2020/6/22/get-june-days/server.js")(
+    juneContext
+  ),
+  "/2020/6/27/get-june-posts": require("./2020/6/27/get-june-posts/server.js")(
+    juneContext
+  ),
+  "/2020/6/27/get-posts": require("./2020/6/27/get-posts/server.js")(
+    juneContext
+  ),
   "/2020/6/27/homepage": require("./2020/6/27/homepage/server.js")(juneContext),
-  "/2020/6/28/actor-rules": require("./2020/6/28/actor-rules/server.js")(juneContext),
-}
+  "/2020/6/28/actor-rules": require("./2020/6/28/actor-rules/server.js")(
+    juneContext
+  )
+};
 
 const handle404error = res => {
   res.writeHead(404, { "Content-Type": "text/html" });
@@ -39,10 +51,14 @@ const main = async () => {
         handleMayUrl(req, res);
         return;
       }
-    
-      // HARDCODED: this if statement
+
+      // HARDCODED: these if statements
       if (req.url === "/2020/6/27/echo-ip") {
         require("./2020/6/27/echo-ip/server.js")(juneContext)(req, res);
+        return;
+      }
+      if (req.url.startsWith("/2020/6/29/url-tail")) {
+        require("./2020/6/29/url-tail/server.js")(juneContext)(req, res);
         return;
       }
 
