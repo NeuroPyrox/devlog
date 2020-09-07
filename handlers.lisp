bb45a4cab8fa54f661cd58f6518483aa98d300ed
Glitch (neuropyrox) <none>
2020-09-07 19:52:43 +0000

    Refactor lambda evaluation

--- handlers.lisp
+++ handlers.lisp
@@ -1,6 +1,7 @@
 (handlers
   (htmlBuilder "2020/8/25/homepageV3.js" "/")
   (html        "genetic-songs.html"      "/wip/genetic-songs")
+  (html        "gas-monad-ideas.html"    "/2020/9/5/gas-monad-ideas")
   (html        "weighted-tree-push.html" "/2020/9/4/weighted-tree-push")
   (html        "string-list.html"        "/2020/9/3/string-list")
   (json        "sexpr-mutations.js"      "/2020/8/29/sexpr-mutations")
