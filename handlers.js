b55380aa989e3150761894c7f4fbdc31174cd3f9
Alex Montoya <NeuroPyrox@Gmail.com>
2020-09-28 18:55:11 -0600

    Refactor (#46)
    * Use applicative parsing for handlers
    
    * Tiny fix
    
    * Refactor
    
    Co-authored-by: Glitch (neuropyrox) <none>

--- handlers.js
+++ handlers.js
@@ -1,151 +1,110 @@
 "use strict";
 
 const fs = require("fs");
+const P = require("./parsers.js");
 
-const assert = condition => {
-  if (!condition) {
-    throw "Assertion Error!";
-  }
+// Each function here returns a list of pairs [[path, handler], ...]
+const handlerTypes = {
+  html: (path, resource) => [
+    [
+      path,
+      async (req, res) => {
+        const stat = await fs.promises.stat(resource);
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+          "Content-Length": stat.size
+        });
+        fs.createReadStream(resource).pipe(res);
+      }
+    ]
+  ],
+  htmlBuilder: (path, resource) => {
+    const htmlBuilder = require(resource);
+    return [
+      [
+        path,
+        async (req, res) => {
+          res.writeHead(200, {
+            "Content-Type": "text/html"
+          });
+          res.write(await htmlBuilder());
+          res.end();
+        }
+      ]
+    ];
+  },
+  json: (path, resource) => {
+    const jsonBuilder = require(resource);
+    return [
+      [
+        path,
+        async (req, res) => {
+          res.writeHead(200, {
+            "Content-Type": "application/json"
+          });
+          res.write(JSON.stringify(await jsonBuilder()));
+          res.end();
+        }
+      ]
+    ];
+  },
+  handlerMap: (path, resource) =>
+    Object.entries(require(resource)).map(([key, value]) => [
+      path + key,
+      value
+    ]),
+  server: (path, resource) => [[path, require(resource)(path)]]
 };
 
-const isLetter = letter =>
-  letter.length === 1 &&
-  (("a" <= letter && letter <= "z") || ("A" <= letter && letter <= "Z"));
+// It's probably better to encode handlers.lisp as json, but I wanted to have fun
+const handlersParser = P.inParentheses(
+  P.skipString("handlers").applyRight(
+    P.many(
+      P.skipString("\n  ").applyRight(
+        P.inParentheses(
+          P.stringOf(
+            char => ("a" <= char && char <= "z") || ("A" <= char && char <= "Z")
+          )
+            .map(type => source => path =>
+              handlerTypes[type](
+                path,
+                source[0] === "/"
+                  ? `.${path}${source}`
+                  : `.${path.slice(0, path.lastIndexOf("/"))}/${source}`
+              )
+            )
+            .applyLeft(P.skipSpaces1)
+            .apply(P.simpleString)
+            .applyLeft(P.skipSpaces1)
+            .apply(P.simpleString)
+        )
+      )
+    ).map(listOfListOfPairs => listOfListOfPairs.flat())
+  )
+);
 
-const parseLetters = (offset, letters) => {
-  var end = offset;
-  while (isLetter(letters[end])) {
-    ++end;
-  }
-  const result = letters.slice(offset, end);
-  offset = end;
-  return { offset, result };
-};
-
-const parseString = (offset, string) => {
-  assert(string[offset++] === '"');
-  var end = offset;
-  while (string[end] !== '"') {
-    ++end;
-  }
-  const result = string.slice(offset, end);
-  offset = ++end;
-  return { offset, result };
-};
-
-const parseSpaces = (offset, spaces) => {
-  assert(spaces[offset++] === " ");
-  while (spaces[offset] === " ") {
-    ++offset;
-  }
-  return { offset };
-};
-
-const parseHandler = (offset, post) => {
-  assert(post[offset++] === "(");
-  var { offset, result: type } = parseLetters(offset, post);
-  var { offset } = parseSpaces(offset, post);
-  var { offset, result: source } = parseString(offset, post);
-  var { offset } = parseSpaces(offset, post);
-  var { offset, result: path } = parseString(offset, post);
-  assert(post[offset++] === ")");
-  return { offset, result: [type, source, path] };
-};
-
-const parseHandlers = handlers => {
-  assert(handlers.slice(0, 12) === "(handlers\n  ");
-  var { offset, result: handler } = parseHandler(12, handlers);
-  const result = [handler];
-  while (handlers[offset] !== ")") {
-    assert(handlers[offset++] === "\n");
-    assert(handlers[offset++] === " ");
-    assert(handlers[offset++] === " ");
-    var { offset, result: handler } = parseHandler(offset, handlers);
-    result.push(handler);
-  }
-  assert(++offset === handlers.length);
-  return result;
-};
-
-const createHtmlEndpoint = resource => async (req, res) => {
-  const stat = await fs.promises.stat(resource);
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-    "Content-Length": stat.size
+const handlersPromise = fs.promises
+  .readFile("handlers.lisp", "utf8")
+  .then(string => {
+    const [result, index] = handlersParser.parse(string, 0).unwrap();
+    if (index !== string.length) {
+      throw "Didn't parse whole file";
+    }
+    return result;
   });
-  fs.createReadStream(resource).pipe(res);
-};
 
-const createHtmlBuilderEndpoint = resource => {
-  const htmlBuilder = require(resource);
-  return async (req, res) => {
-    res.writeHead(200, {
-      "Content-Type": "text/html"
-    });
-    res.write(await htmlBuilder());
-    res.end();
-  };
+module.exports = async (req, res, handle404error) => {
+  const handlers = await handlersPromise;
+  // This is inefficient
+  for (const [key, value] of handlers) {
+    if (req.url === key) {
+      return value(req, res);
+    }
+  }
+  for (const [key, value] of handlers) {
+    if (req.url.startsWith(key)) {
+      return value(req, res);
+    }
+  }
+  handle404error(req, res);
 };
-
-const createJsonEndpoint = resource => {
-  const jsonBuilder = require(resource);
-  return async (req, res) => {
-    res.writeHead(200, {
-      "Content-Type": "application/json"
-    });
-    res.write(JSON.stringify(await jsonBuilder()));
-    res.end();
-  };
-};
-
-module.exports = (() => {
-  const promise = (async () => {
-    const handlersList = parseHandlers(
-      await fs.promises.readFile("handlers.lisp", "utf8")
-    );
-    const endpoints = {};
-    const servers = {};
-    for (const [type, source, path] of handlersList) {
-      assert(path[0] === "/");
-      const resource =
-        source[0] === "/"
-          ? `.${path}${source}`
-          : `.${path.slice(0, path.lastIndexOf("/"))}/${source}`;
-      switch (type) {
-        case "html":
-          endpoints[path] = createHtmlEndpoint(resource);
-          break;
-        case "htmlBuilder":
-          endpoints[path] = createHtmlBuilderEndpoint(resource);
-          break;
-        case "json":
-          endpoints[path] = createJsonEndpoint(resource);
-          break;
-        case "handlerMap":
-          for (const [key, value] of Object.entries(require(resource))) {
-            endpoints[path + key] = value;
-          }
-          break;
-        case "server":
-          servers[path] = require(resource)(path);
-          break;
-        default:
-          assert(false);
-      }
-    }
-    return [endpoints, servers];
-  })();
-  return async (req, res, handle404error) => {
-    const [endpoints, servers] = await promise;
-    const endpoint = endpoints[req.url];
-    if (endpoint !== undefined) {
-      return endpoint(req, res);
-    }
-    for (const [key, value] of Object.entries(servers)) {
-      if (req.url.startsWith(key)) {
-        return value(req, res);
-      }
-    }
-    handle404error(req, res);
-  };
-})();
