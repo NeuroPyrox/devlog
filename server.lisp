e7f014ea18dc0df2dea3751987c266bc11f6662c
Alex Montoya <NeuroPyrox@Gmail.com>
2020-10-04 01:47:54 -0600

    Remove unused code (#53)
    Co-authored-by: Glitch (neuropyrox) <none>

--- server.lisp
+++ server.lisp
@@ -7,7 +7,6 @@
   (html        "sexpr-parser.html"       "/2020/9/18/sexpr-parser")
   (json        "sql-counter.js"          "/2020/9/16/sql-counter")
   (html        "genetic-songs.html"      "/2020/9/13/genetic-songs")
-  (html        "gas-monad-ideas.html"    "/2020/9/5/gas-monad-ideas")
   (html        "weighted-tree-push.html" "/2020/9/4/weighted-tree-push")
   (html        "string-list.html"        "/2020/9/3/string-list")
   (json        "sexpr-mutations.js"      "/2020/8/29/sexpr-mutations")
