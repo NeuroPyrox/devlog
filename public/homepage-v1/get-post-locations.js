6039da18d6dd650f996b53261ec20b170213b478
Glitch (neuropyrox) <none>
2021-01-04 23:30:13 +0000

    Flatten May file structure

--- public/homepage-v1/get-post-locations.js
+++ public/homepage-v1/get-post-locations.js
@@ -4,20 +4,11 @@
 
 const postFinder = require("../post-finder.js");
 
-// TODO single source of truth
-const oldPosts = [
-  { day: "16 May 2020", title: "Simple Homepage", href: "/2020/5/16/" },
-  { day: "15 May 2020", title: "May Folders", href: "/2020/5/15/" },
-  { day: "14 May 2020", title: "Hello World", href: "/2020/5/14/" }
-];
-
-module.exports = async () => {
-  const mayPosts = (await postFinder())
-    .map(({ day, title }) => ({
+module.exports = async () =>
+  (await postFinder())
+    .map(({ day, href }) => ({
       date: `${day} May 2020`,
-      title,
-      href: `/${title}`
+      title: href,
+      href
     }))
     .reverse();
-  return mayPosts.concat(oldPosts);
-};
