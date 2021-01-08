79deecb5d08dc477e4a3f580b3c5acd1d76e2f9f
Alex Montoya <NeuroPyrox@Gmail.com>
2021-01-08 14:34:44 -0700

    Referrer Policy (#108)
    Co-authored-by: Glitch (neuropyrox) <none>

--- server.js
+++ server.js
@@ -91,6 +91,7 @@ const handlersPromise = fs.promises
 
 const handleHttps = async (req, res) => {
   res.setHeader("x-frame-options", "deny");
+  res.setHeader("referrer-policy", "no-referrer");
   // 31536000 seconds is one non-leap year
   res.setHeader("strict-transport-security", "max-age=31536000");
   res.setHeader("expect-ct", "max-age=31536000, enforce");
