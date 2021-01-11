6ccdd33bb48dfcf1795c7411757a0f130b36e074
Glitch (neuropyrox) <none>
2021-01-11 03:31:51 +0000

    Favicon

--- public/cookie-counter.js
+++ public/cookie-counter.js
@@ -1,11 +1,9 @@
 "use strict";
 
 // TODO make the parser deal with several cookies in the header
-// TODO go over my security checklist
 
 const P = require("../parsers.js");
 
-// I could've used a library, but I'm doing things from scratch for educational purposes
 const cookieParser = P.string("__Secure-counter=")
   .skipLeft(P.any)
   .map(parseInt);
