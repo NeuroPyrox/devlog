1a81bb51e6f96f64747b5f256d04d5c73d293200
Alex Montoya <NeuroPyrox@Gmail.com>
2021-01-11 17:31:04 -0700

    More favicons (#119)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/apple-touch-icon.png.js
+++ public/apple-touch-icon.png.js
@@ -4,7 +4,7 @@ const P = require("../parsers.js");
 
 module.exports = P.end.map(_ => (req, res) => {
   res.writeHead(302, {
-    Location: `https://cdn.glitch.com/ef6d7e3d-88ee-4914-a43c-ca8be9bdf01f%2Ffavicon.ico?v=1610405131185`
+    Location: `https://cdn.glitch.com/ef6d7e3d-88ee-4914-a43c-ca8be9bdf01f%2Fapple-touch-icon.png?v=1610405083959`
   });
   res.end();
 });
---
+++ public/site.webmanifest.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = () => ({
+  name: "NeuroPyrox",
+  short_name: "NeuroPyrox",
+  icons: [
+    {
+      src: "/android-chrome-192x192.png",
+      sizes: "192x192",
+      type: "image/png"
+    },
+    {
+      src: "/android-chrome-384x384.png",
+      sizes: "384x384",
+      type: "image/png"
+    }
+  ],
+  theme_color: "#006600",
+  background_color: "#006600",
+  display: "standalone"
+});
