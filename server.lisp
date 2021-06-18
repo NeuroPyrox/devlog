7eaadd28da10886054d1f2985307a71a35a90f69
Glitch (neuropyrox) <none>
2021-06-18 17:40:21 +0000

    Clear Delete Insert List

--- server.lisp
+++ server.lisp
@@ -32,6 +32,7 @@
   (json        /post-locations              .js)
   (json        /post-finder                 .js)
   (json        /get-may-days                .js)
+  (html        /clear-delete-insert-list    .html)
   (html        /expandable-list             .html)
   (html        /democracy-v3                .html)
   (html        /democracy-v1                .html)
