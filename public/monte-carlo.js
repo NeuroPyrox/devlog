86bca6ea0f57c4308de92e442aeeada00b21e05b
Alex Montoya <NeuroPyrox@Gmail.com>
2022-04-03 06:03:56 -0600

    Monte Carlo (#575)
    Co-authored-by: Glitch (neuropyrox) <none>

---
+++ public/monte-carlo.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// The input is a 95% confidence interval
+const gaussian = (low, high) => {
+  const nSamples = 10;
+  const uniformVariance = 1 / 12;
+  const sumVariance = uniformVariance * nSamples;
+  const sum =
+    [...Array(nSamples)].reduce((x) => x + Math.random(), 0) - nSamples / 2;
+  const average = (low + high) / 2;
+  const range = high - low;
+  return average + (sum * range) / (4 * sumVariance ** 0.5);
+};
+
+const model = () => {
+  const workRoi = gaussian(1.04, 1.5);
+  const lifeExpectancy = gaussian(50, 120);
+  const productivity = gaussian(0.05, 0.9);
+  return workRoi ** (lifeExpectancy * productivity);
+};
+
+module.exports = () => [...Array(100)].map(() => model());
