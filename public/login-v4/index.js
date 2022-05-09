b1c76621360d5f2940a487f7b57fa5d01eee2464
Alex Montoya <NeuroPyrox@Gmail.com>
2022-05-09 05:59:43 -0600

    Move util to separate file (#601)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/index.js
+++ public/login-v4/index.js
@@ -3,4 +3,6 @@
 const P = require("../../parsers.js");
 const htmlHandler = require("../../lib/html-handler.js");
 
-module.exports = P.end.map((_) => htmlHandler(`${__dirname}/index.html`));
+module.exports = P.end
+  .map((_) => htmlHandler(`${__dirname}/index.html`))
+  .or(P.endIn("/util.js").map(() => htmlHandler(`${__dirname}/util.js`)));
