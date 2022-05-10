2ffee97786f4614f3660e00d35d4b736461b1cb6
Alex Montoya <NeuroPyrox@Gmail.com>
2022-05-10 10:28:08 +0000

    Redirect (#602)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/index.js
+++ public/login-v4/index.js
@@ -4,5 +4,12 @@ const P = require("../../parsers.js");
 const htmlHandler = require("../../lib/html-handler.js");
 
 module.exports = P.end
-  .map((_) => htmlHandler(`${__dirname}/index.html`))
+  .map(() => (req, res) => {
+    console.log(req);
+    res.writeHead(307, {
+      Location: req.url + "/",
+    });
+    res.end();
+  })
+  .or(P.endIn("/").map(() => htmlHandler(`${__dirname}/index.html`)))
   .or(P.endIn("/util.js").map(() => htmlHandler(`${__dirname}/util.js`)));
