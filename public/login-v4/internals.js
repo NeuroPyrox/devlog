fce81dbc2bcdda8c3a572afc71e9556d16b795d6
Glitch (neuropyrox) <none>
2022-06-26 10:11:04 +0000

    Comments

--- public/login-v4/internals.js
+++ public/login-v4/internals.js
@@ -17,38 +17,38 @@ const k = (x) => () => x;
 // A (weak x) y means a [WeakRef] y where [y.deref() === undefined] or [y.deref()] is an x.
 //   ((weak) y) means a (weak x) y.
 //   A (weak x) means a (weak x) y.
+//   A (weak)   means a (weak x) y.
 //   (y is live) means [y.deref() !== undefined].
 //   (y is dead) means [y.deref() === undefined].
 //   (y is dead) implies (y will never be live).
+//   (y has z) means [y.deref() === z].
 //   (y has garbage) means (y is live and [y.deref()] is garbage).
 //   (y has garbage) implies (y will always (have garbage or be dead)).
 //   (y is strictly live) means ((y is live) and (y doesn't have garbage)).
 //   (y is strictly live) xor (y has garbage) xor (y is dead).
 //   (y has garbage) doesn't equate to (y is garbage).
 // (garbage collection) means (some (weak x) that have garbage become dead).
-// A sink   means a weak ([EventSink]   or [BehaviorSink]).
-// A source means a weak ([EventSource] or [BehaviorSource]).
+// A sink   means a (weak ([EventSink]   or [BehaviorSink])).
+// A source means a (weak ([EventSource] or [BehaviorSource])).
 // (sink   x is a parent of sink   y) means (x is live and y is live and [x.deref().#children] strongly references [y.deref()]).
 // (source x is a parent of source y) means (x is live and y is live and [y.deref().#parents]  strongly references [x.deref()]).
 // (x is a child  of y) means (y is a parent of x).
-// TODO update mentions of "weak"
-// TODO sink and source properties
 // (x is a nested parent of y) means (y is a nested child of x) means (x is a parent of (y or one of y's nested parents)).
-//   (sink   x is a nested parent of sink   y) implies (x and y are both an [EventSink])   xor (x and y are both a [BehaviorSink]).
-//   (source x is a nested parent of source y) implies (x and y are both an [EventSource]) xor (x and y are both a [BehaviorSource]).
-// (The sink of a live source o) means [o.#weakSink.deref()].
-// (The source of a live sink i) means ((the unique live source o where the sink of o is i) or ([undefined] if no such live source exists)).
+//   (sink   x is a nested parent of sink   y) implies (x and y both have an [EventSink])   xor (x and y both have a [BehaviorSink]).
+//   (source x is a nested parent of source y) implies (x and y both have an [EventSource]) xor (x and y both have a [BehaviorSource]).
+// (The sink of a live source o) means [o.deref().#weakSink].
+// (The source of a live sink i) means ((the unique live source whose sink is i) or (a dead (weak) if no such live source exists)).
 // (Live sink i is the sink of live source o) iff (o is the source of i).
 // (sink i pairs with source o) means (source o pairs with sink i) means ((i is the sink of o) or (o is the source of i)).
-//   An [EventSink]      can only pair with a weak [EventSource].
-//   An [EventSource]    can only pair with a weak [EventSink].
-//   A  [BehaviorSink]   can only pair with a weak [BehaviorSource].
-//   A  [BehaviorSource] can only pair with a weak [BehaviorSink].
-//   [undefined] can pair with an ([EventSink] or [EventSource] or [BehaviorSink] or [BehaviorSource]).
-// An event    (i,o) is a weak [EventSink]    i and a weak [EventSource]    o where i pairs with o.
-// A  behavior (i,o) is a weak [BehaviorSink] i and a weak [BehaviorSource] o where i pairs with o.
+//   A (weak [EventSink])      can only pair with a (weak [EventSource]).
+//   A (weak [EventSource])    can only pair with a (weak [EventSink]).
+//   A (weak [BehaviorSink])   can only pair with a (weak [BehaviorSource]).
+//   A (weak [BehaviorSource]) can only pair with a (weak [BehaviorSink]).
+// An event    (i,o) is a (weak [EventSink])    i and a (weak [EventSource])    o where i pairs with o.
+// A  behavior (i,o) is a (weak [BehaviorSink]) i and a (weak [BehaviorSource]) o where i pairs with o.
 // A reactive (i,o) is an event (i,o) or a behavior (i,o).
 //   Equivalently, ((i,o) is a reactive) iff (i is a sink, o is a source, and i pairs with o).
+// TODO sink and source properties
 // TODO what about source references?
 // TODO Possible parent relationships:
 //   ([undefined], unpullable) parent of ([undefined], unpullable)
