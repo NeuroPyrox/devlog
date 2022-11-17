765f59bc701e1e48c4c67b3a8799bf454d41b7f0
Alex Montoya <NeuroPyrox@Gmail.com>
2022-11-17 02:36:31 -0700

    Refactor [output] (#845)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/pull.js
+++ public/login-v4/pull.js
@@ -1,43 +1,17 @@
-import { assert, nothing, createGeneratorMonad, once } from "./util.js";
+import { assert, createGeneratorMonad, once } from "./util.js";
 import {
-  lazyConstructor,
   lazyLoop,
   delayConstructionDuring,
 } from "./lazyConstructors.js";
-import { newEventPair } from "./internals.js";
-
-import { pure } from "./push.js"; // Circular dependency
-
-// TODO garbage collection.
-const outputs = [];
 
 const key = Symbol();
 
 const context = {
-  // TODO update comments
-  // To stop the output, call [source.getWeakSink().deref()?.deactivate()].
-  // When the return value loses all its references,
-  // we assert that the sink is not active,
-  // and later the output gets garbage collected.
-  // Implementation-wise, there's no need to put this function
-  // in the Pull monad, but we do it to make the semantics cleaner
-  // and for the ability to control when the output starts.
-  output: (parent, handle) =>
-    lazyConstructor((parentSource) => {
-      const [sink, source] = newEventPair([parentSource], (value) => {
-        lazyConstructor(() => handle(value));
-        return pure(nothing);
-      });
-      sink.activate();
-      outputs.push(source); // TODO remove
-      return source;
-    }, parent),
   loop: lazyLoop,
   getKey: () => key,
 };
 
 const [runPullMonad, monadicMethod] = createGeneratorMonad();
-export const output = monadicMethod("output");
 export const loop = monadicMethod("loop")();
 const getKey = monadicMethod("getKey")();
 
