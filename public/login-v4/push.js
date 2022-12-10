e4985492ee653de14941de144d2fd5347ab1f325
Glitch (neuropyrox) <none>
2022-12-10 12:54:48 +0000

    Refactor

--- public/login-v4/push.js
+++ public/login-v4/push.js
@@ -3,6 +3,17 @@ import { nothing } from "./util.js";
 import { delayConstructionDuring } from "./lazyConstructors.js";
 import { pull } from "./pull.js";
 
+// This is supposed to be an encapsulated sum type for context mutators.
+// The encapsulation prevents new types of mutators from being made.
+const key = Symbol();
+export const pure = (value) => ({ [key]: (context) => value });
+export const liftPull = (monadicValue) => ({
+  [key]: (context) => context.liftPull(monadicValue),
+});
+export const enqueueBehaviorValue = (sink, value) => ({
+  [key]: (context) => context.enqueueBehaviorValue(sink, value),
+});
+
 // We create a new instance of [Context] during every [push] so we can garbage collect [eventValues].
 class Context {
   #eventValues;
@@ -27,6 +38,10 @@ class Context {
     }
     return value.value;
   }
+  
+  doAction(action) {
+    action[key](this);
+  }
 
   // We use "lift" in the name of this method because [Context] resembles a monad.
   // In fact, it used to be a monad before I refactored it.
@@ -46,41 +61,25 @@ class Context {
         heap.push(child);
       }
     }
-    // Propagate
     for (const { sink } of heap) {
       for (const child of sink.push()) {
+        // Mutating the heap while iterating over it.
         heap.push(child);
       }
     }
   }
 }
 
-const key = Symbol();
-export const pure = (value) => ({ [key]: (context) => value });
-export const liftPull = (monadicValue) => ({
-  [key]: (context) => context.liftPull(monadicValue),
-});
-export const enqueueBehaviorValue = (sink, value) => ({
-  [key]: (context) => context.enqueueBehaviorValue(sink, value),
-});
-
 // Delay construction because we don't want to visit newly created reactives.
 export const push = (sink, value) =>
   delayConstructionDuring(() => {
     const context = new Context();
-    const readEvent = (sink) => context.readEvent(sink);
-    context.writeEvent(sink, value);
     const heap = new Heap((a, b) => a.priority < b.priority);
-    for (const child of sink.iterateActiveChildren()) {
+    for (const child of sink.pushValue(context, value)) {
       heap.push(child);
     }
     for (const { sink } of heap) {
-      const value = sink.push(readEvent)[key](context);
-      if (value === nothing) {
-        continue;
-      }
-      context.writeEvent(sink, value);
-      for (const child of sink.iterateActiveChildren()) {
+      for (const child of sink.push(context)) {
         // Mutating the heap while iterating over it.
         heap.push(child);
       }
