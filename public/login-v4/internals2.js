b6ca6034717464733d8fe3fb6f9812f011ca4964
Alex Montoya <NeuroPyrox@Gmail.com>
2022-12-24 06:38:11 -0700

    Make new version of [EventSink] only with public methods (#934)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/internals2.js
+++ public/login-v4/internals2.js
@@ -9,7 +9,9 @@ import { assert, ShrinkingList, derefMany } from "./util.js";
 //   Input:                                                                                                          pushValue
 //   Else:                                                                                                                     push
 
-const { newSink } = (() => {
+// In this file "o" means public and "i" means private. Think of them as outer and inner.
+
+const sink = (() => {
   const p = Symbol();
 
   class Sink {
@@ -78,5 +80,87 @@ const { newSink } = (() => {
     }
   }
 
-  return { newSink: (...args) => new Wrapper(...args) };
+  return {};
 })();
+
+const eventSink = sink.iSubclass((o, i) => ({
+  constructor(
+    weakParents,
+    push,
+    { unsubscribe = () => {}, enforceManualDeactivation = false }
+  ) {
+    super(weakParents);
+    this[i].activeChildren = new ShrinkingList();
+    this[i].activeChildRemovers = [];
+    this[i].enforceManualDeactivation = enforceManualDeactivation; // Only used for output events.
+    this[i].push = push;
+    this[i].unsubscribe = unsubscribe; // Only used for input events.
+  },
+  o: {
+    activate() {
+      assertConstructing();
+      if (this[i].activeChildRemovers.length !== 0) {
+        // Filters out all sinks that are already active, except for inputs.
+        return;
+      }
+      this[i].forEachParent((parent) => {
+        parent[o].activate();
+        this[i].activeChildRemovers.push(
+          new WeakRef(parent[i].activeChildren.add(this))
+        );
+      });
+    },
+    deactivate() {
+      assertConstructing();
+      for (const deactivator of this[i].activeChildRemovers) {
+        deactivator.deref()?.removeOnce();
+      }
+      this[i].activeChildRemovers = [];
+      this[i].forEachParent((parent) => {
+        if (parent[i].activeChildren.isEmpty()) {
+          // From one to zero children.
+          parent[o].deactivate();
+        }
+      });
+    },
+    switch(parentSource) {
+      assertConstructing();
+      const weakParent = parentSource.getWeakSink();
+      // This early exit is an O(# of nested parents) optimization.
+      if (this[i].isFirstParent(weakParent)) {
+        return;
+      }
+      this[o].deactivate();
+      this[i].switch(weakParent); // TODO resolve shadowing
+      const hasActiveChild = !this[i].activeChildren.isEmpty();
+      if (hasActiveChild) {
+        this[o].activate();
+      }
+    },
+    *pushValue(context, value) {
+      assertLazy();
+      context.writeEvent(this, value);
+      yield* this[i].iterateActiveChildren();
+    },
+    *push(context) {
+      assertLazy();
+      if (context.isWritten(this)) {
+        // Guards against being called more than once.
+        // We don't need any fancy algorithmic optimizations
+        // because [EventSink]s have at most 2 parents.
+        return;
+      }
+      const action = this[i].push(
+        ...this[i].mapWeakParents((weakParent) =>
+          context.readEvent(weakParent.deref())
+        )
+      );
+      const value = context.doAction(action);
+      context.writeEvent(this, value);
+      if (value !== nothing) {
+        yield* this[i].iterateActiveChildren();
+      }
+    },
+  },
+  i: {},
+}));
