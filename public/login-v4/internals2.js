9df8d3d2177cbdc834f4ef3ffe39b9d4340dffc9
Glitch (neuropyrox) <none>
2022-12-25 09:52:19 +0000

    Redefine [sink]

--- public/login-v4/internals2.js
+++ public/login-v4/internals2.js
@@ -9,77 +9,58 @@ import { assert, ShrinkingList, derefMany } from "./util.js";
 //   Input:                                                                                                          pushValue
 //   Else:                                                                                                                     push
 
-const sink = (() => {
-  const p = Symbol();
-
-  class Sink {
-    constructor(weakParents) {
-      this.#setWeakParents(weakParents);
-      this.children = new ShrinkingList();
-      this.priority =
-        Math.max(
-          -1,
-          ...derefMany(weakParents).map((parent) => parent[p].getPriority())
-        ) + 1;
-    }
-
-    switch(weakParent) {
-      assert(this.weakParents.length <= 1);
-      this.removeFromParents();
-      this.#setWeakParents([weakParent]);
-      weakParent.deref()?.[p]?.#switchPriority(this.priority);
-    }
-
-    mapWeakParents(f) {
-      return this.weakParents.map(f);
-    }
-
+const sink = privatelyInheritableClass((k) => ({
+  constructor(weakParents) {
+    this[k].setWeakParents(weakParents);
+    this[k].children = new ShrinkingList();
+    this[k].priority =
+      Math.max(
+        -1,
+        ...derefMany(weakParents).map((parent) => parent[k].getPriority())
+      ) + 1;
+  },
+  public: {
+    switchParent(weakParent) {
+      assert(this[k].weakParents.length <= 1);
+      this[k].removeFromParents();
+      this[k].setWeakParents([weakParent]);
+      weakParent.deref()?.[k]?.switchPriority(this[k].priority);
+    },
     // Used for early exits from [EventSink.switch]
     isFirstParent(weakParent) {
-      return weakParent.deref() === this.weakParents[0]?.deref();
-    }
-
+      return weakParent.deref() === this[k].weakParents[0]?.deref();
+    },
     forEachParent(f) {
-      derefMany(this.weakParents).forEach(f);
-    }
-
+      derefMany(this[k].weakParents).forEach(f);
+    },
     getPriority() {
-      return this.priority;
-    }
-
+      return this[k].priority;
+    },
     // Removes all strong references from the [children] of [weakParents].
     removeFromParents() {
-      for (const weakParentLink of this.weakParentLinks) {
+      for (const weakParentLink of this[k].weakParentLinks) {
         weakParentLink.deref()?.removeOnce();
       }
-    }
-
-    #setWeakParents(weakParents) {
-      this.weakParents = weakParents;
-      this.weakParentLinks = derefMany(weakParents).map(
-        (parent) => new WeakRef(parent[p].children.add(this))
+    },
+  },
+  private: {
+    setWeakParents(weakParents) {
+      this[k].weakParents = weakParents;
+      this[k].weakParentLinks = derefMany(weakParents).map(
+        (parent) => new WeakRef(parent[k].children.add(this))
       );
-    }
-
+    },
     // TODO custom error message for infinite recursion
-    #switchPriority(childPriority) {
-      if (childPriority <= this.priority) {
-        this.priority = childPriority - 1;
-        this.forEachParent((parent) =>
-          parent[p].#switchPriority(this.priority)
+    switchPriority(childPriority) {
+      if (childPriority <= this[k].priority) {
+        this[k].priority = childPriority - 1;
+        this[k].forEachParent((parent) =>
+          parent[k].switchPriority(this[k].priority)
         );
       }
-    }
-  }
-
-  class Wrapper {
-    constructor(...args) {
-      this[p] = new Sink(...args);
-    }
-  }
-
-  return {};
-})();
+    },
+  },
+}));
 
 const eventSink = sink.privateSubclass((k) => ({
   constructor(
