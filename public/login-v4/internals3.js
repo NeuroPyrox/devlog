c12d073a202d4b537eacdcb17a138d87e416c0a4
Glitch (neuropyrox) <none>
2023-01-22 11:26:14 +0000

    Define event interfaces

--- public/login-v4/internals3.js
+++ public/login-v4/internals3.js
@@ -1,7 +1,18 @@
 import { assert, ShrinkingList, derefMany, memoize, nothing } from "./util.js";
 import * as Push from "./push.js";
 
-// TODO define interfaces
+// Never                                                               getPriority                        recursivelyWait recursivelyUnwait
+// Input                                                               getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy
+// Output           setParents removeParents readParents forEachParent getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy push
+// Map              setParents removeParents readParents forEachParent getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy push
+// Filter           setParents removeParents readParents forEachParent getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy push
+// Merge            setParents removeParents readParents forEachParent getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy push
+// SwitchModulator  setParents removeParents readParents forEachParent getPriority                        recursivelyWait recursivelyUnwait destroy push
+// StepperModulator setParents removeParents readParents forEachParent getPriority                        recursivelyWait recursivelyUnwait destroy push
+// SwitchModulatee  setParents removeParents readParents forEachParent getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy push switchPriority
+// DemuxParent      setParents removeParents readParents forEachParent getPriority                        recursivelyWait recursivelyUnwait destroy push
+// DemuxChild                                                          getPriority iterateWaitingChildren recursivelyWait recursivelyUnwait destroy
+// TODO behavior interfaces
 
 class Never {
   getPriority() {
@@ -31,10 +42,8 @@ class Output {
   }
   
   setParents(weakParents) {
-    if (this.weakParentsChildRemovers) {
-      for (const weakChildRemover of this.weakParentsChildRemovers) {
-        weakChildRemover.deref()?.removeOnce();
-      }
+    for (const weakChildRemover of this.weakParentsChildRemovers ?? []) {
+      weakChildRemover.deref()?.removeOnce();
     }
     this.weakParents = weakParents;
     this.weakParentsChildRemovers = derefMany(weakParents).map(
