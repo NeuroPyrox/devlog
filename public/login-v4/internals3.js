011ece6ef452c1b44f1e59b072941c46806221d0
Glitch (neuropyrox) <none>
2023-01-24 11:29:26 +0000

    Start new [Sink] class

--- public/login-v4/internals3.js
+++ public/login-v4/internals3.js
@@ -19,3 +19,42 @@ import * as Push from "./push.js";
 // StepperModulatee                                                    getPriority iterateWaitingChildren
 // Map              addParents removeParents readParents forEachParent getPriority iterateWaitingChildren            wait            unwait destroy push
 // Apply            addParents removeParents readParents forEachParent getPriority iterateWaitingChildren            wait            unwait destroy push
+
+class Sink {
+  // These 3 variables interact with each other a lot.
+  #parents;
+  #weakParentLinks;
+  #children;
+  // This variable is largely independent of the other ones,
+  // but refactoring it into its own class would expose more methods.
+  #priority;
+  
+  constructor(weakParents) {
+    this.#parentInfo = {};
+    this.#weakParentLinks = [];
+    this.#children = new ShrinkingList();
+    this.#priority =
+      Math.max(
+        -1,
+        ...derefMany(weakParents).map((parent) => parent.getPriority())
+      ) + 1;
+    this.addWeakParents(weakParents);
+  }
+
+  addWeakParents(weakParents) {
+    for (const key in weakParents) {
+      if (weakParent.deref() !== undefined) {
+        assert(!(key in this.#parentInfo));
+        this.#parentInfo[key] = {
+          weakRef: weakParents[key],
+          childRemover: parent.#children.add(this);
+        }
+        // TODO finalizer
+      }
+    }
+  }
+  
+  getPriority() {
+    return this.#priority;
+  }
+}
