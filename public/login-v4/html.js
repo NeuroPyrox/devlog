4b26f09ca5af39d01ec4ee526cd9f5b396b47b24
Alex Montoya <NeuroPyrox@Gmail.com>
2023-02-01 04:04:30 -0700

    Create [textInput] (#994)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/html.js
+++ public/login-v4/html.js
@@ -1,10 +1,24 @@
 import { createGeneratorMonad } from "./util.js";
 import * as Pull from "./pull.js";
 
+// TODO GC outputs.
+
 class Context {
-  pull(pullMonadValue) {
-    Pull.pull(pullMonadValue);
+  constructor() {
+    this.nodes = [];
+  }
+  
+  pull(monadicPullValue) {
+    Pull.pull(monadicPullValue);
+  }
+  
+  textInput({setValue}) {
+    const node = document.createElement("input");
+    node.type = "text";
+    Pull.pull(() => setValue.output(value => node.value = value));
+    this.nodes.push(node);
   }
 }
 const [runHtmlMonad, monadicMethod] = createGeneratorMonad();
 export const pull = monadicMethod("pull");
+export const textInput = monadicMethod("textInput");
