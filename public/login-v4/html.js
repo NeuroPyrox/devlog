496201a2b436ecefd6d8e7e9ddc661ca01a9df56
Alex Montoya <NeuroPyrox@Gmail.com>
2023-02-01 05:02:28 -0700

    Create [insertChildren] method of [tbody] (#995)
    Co-authored-by: Glitch (neuropyrox) <none>

--- public/login-v4/html.js
+++ public/login-v4/html.js
@@ -7,16 +7,42 @@ class Context {
   constructor() {
     this.nodes = [];
   }
-  
+
   pull(monadicPullValue) {
     Pull.pull(monadicPullValue);
   }
-  
-  textInput({setValue}) {
+
+  textInput({ setValue }) {
     const node = document.createElement("input");
     node.type = "text";
-    Pull.pull(() => setValue.output(value => node.value = value));
+    Pull.pull(() => setValue.output((value) => (node.value = value)));
     this.nodes.push(node);
+  }
+
+  // TODO synchronize outputs.
+  tbody({ insertChildren, removeChild, setInnerHtml }) {
+    const node = document.createElement("tbody");
+    let afterInsertChildren;
+    Pull.pull(function* () {
+      insertChildren = insertChildren.map(([index, monadicHtmlValue]) => {
+        const nestedContext = new Context();
+        const result = runHtmlMonad(nestedContext, monadicHtmlValue());
+        return [result, index, nestedContext.nodes];
+      });
+      insertChildren.output(([, index, children]) => {
+        if (index === node.children.length) {
+          for (const child of children) {
+            node.appendChild(child);
+          }
+        } else {
+          const end = node.children[index];
+          for (const child of children) {
+            node.insertBefore(child, end);
+          }
+        }
+      });
+      afterInsertChildren = insertChildren.map(([result]) => result);
+    });
   }
 }
 const [runHtmlMonad, monadicMethod] = createGeneratorMonad();
