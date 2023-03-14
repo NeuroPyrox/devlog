7ac36a3d1a627560a25ed0086c80c4c14fa33eb4
Glitch (neuropyrox) <none>
2023-03-14 04:58:49 +0000

    Formatting

--- public/marriott-spreadsheet-converter/maybe.js
+++ public/marriott-spreadsheet-converter/maybe.js
@@ -1,14 +1,15 @@
+// TODO use classes
 export const just = (x) => ({
   map: (f) => just(f(x)),
   chain: (f) => f(x),
-  or: (_) => just(x),
-  unwrap: (_) => x,
+  or: () => just(x),
+  unwrap: () => x,
 });
 
 // TODO better error message for [unwrap]
 export const nothing = {
-  map: (_) => nothing,
-  chain: (_) => nothing,
+  map: () => nothing,
+  chain: () => nothing,
   or: (f) => f(),
 };
 
